7a87759fcf649ae35addb29ada37774e652fd41d
Laura R <ringlstetterlaura@gmail.com>
2025-04-16 13:12:19 +0200

    anpassung db

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    uri: process.env.DATABASE_URL,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
